c811216e4da4e77bc6c1452c6e43866812cb3790
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-01 22:04:59 -0600

    Diagraming out the problem

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,8 +2,20 @@ require 'date'
 
 class Enigma
 
-  def encrypt(message, key = "12345", date = Date.today)
+  # def initialize
+  #   @date = date
+  # end
+
+  def encrypt(message, key = '12345', date = Date.today.strftime('%d%m%y'))
 
   end
+
+
+  def date_manipulation
+
+  end
+
+
+
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,12 +12,14 @@ class EngimaTest < Minitest::Test
 
   def test_it_can_encrypt
     e = Enigma.new
-    my_message = "this is so secret ..end.."
+    my_message = 'this is so secret ..end..'
     output_1 = e.encrypt(my_message, "12345", Date.today)
-    assert_equal "dkjsfhskdjhf", output_1
+    assert_equal 'dkjsfhskdjhf', output_1
     # above string will be determined in future
     output_2 = e.encrypt(my_message)
     #key and date are optional (generate random key and use today's date)
-    assert_equal "dskjhfsdkjhf", output_2
+    assert_equal 'dskjhfsdkjhf', output_2
   end
 end
+
+date = date.parse(11-01-2018)
