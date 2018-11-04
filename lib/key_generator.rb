e8054a9e071f8fae8f85c5ba4d5a6340539ed4fa
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-04 10:22:18 -0700

    Integrate: create_key

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,9 +1,9 @@
+require 'securerandom'
+
 class KeyGenerator
+
+
+  def create_key
+    SecureRandom.random_number(99999).to_s.rjust(5, "0")
+  end
 end
-
-
-# Key Generator
-# Account for: leading zeros
-# Random Number Generator
-
-
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -1,20 +1,18 @@
 require './test/test_helper'
-require './lib/offset'
 require './lib/key_generator'
 
 class KeyGeneratorTest < Minitest::Test
 
   def test_it_exists
     key = KeyGenerator.new
-    assert_equal KeyGenerator, key
+    assert_instance_of KeyGenerator, key
   end
 
   def test_it_creates_a_key
-skip
     key = KeyGenerator.new
     new_key = key.create_key
-    assert_equal String, new_key
     assert_equal 5, new_key.length
+    
   end
 
   def test_it_generates_a_random_key
@@ -26,6 +24,6 @@ skip
     new_key_2 = key_2.create_key
     new_key_3 = key_3.create_key
     refute new_key_1 != new_key_2
-    refute new_key_2 != new_key_3    
+    refute new_key_2 != new_key_3
   end
 end
