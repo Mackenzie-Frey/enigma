f062429dfd5b0891a0748a2b561f90f4400ebde1
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-04 09:59:08 -0700

    Test: generates_a_random_key, creates_a_key, it_exists

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -4,4 +4,28 @@ require './lib/key_generator'
 
 class KeyGeneratorTest < Minitest::Test
 
+  def test_it_exists
+    key = KeyGenerator.new
+    assert_equal KeyGenerator, key
+  end
+
+  def test_it_creates_a_key
+skip
+    key = KeyGenerator.new
+    new_key = key.create_key
+    assert_equal String, new_key
+    assert_equal 5, new_key.length
+  end
+
+  def test_it_generates_a_random_key
+skip
+    key_1 = KeyGenerator.new
+    key_2 = KeyGenerator.new
+    key_3 = KeyGenerator.new
+    new_key_1 = key_1.create_key
+    new_key_2 = key_2.create_key
+    new_key_3 = key_3.create_key
+    refute new_key_1 != new_key_2
+    refute new_key_2 != new_key_3    
+  end
 end
