83c577ea34ce7d5cb5d9cd44850528f101fe1a3d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 15:48:58 -0700

    Shift class absorbs offset class

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,11 +1,14 @@
 require 'securerandom'
+require 'date'
 
 class Shift
 
-  attr_reader :key
+  attr_reader :key,
+              :date
 
-  def initialize
+  def initialize(date = Date.today.strftime('%d%m%y'))
     @key = SecureRandom.random_number(99999).to_s.rjust(5, "0")
+    @date = date
   end
 
   def a_key
@@ -22,6 +25,35 @@ class Shift
 
   def d_key
     (@key[3] + @key[4]).to_i
+  end
+
+  def date_squared
+    @date.to_i ** 2
+  end
+
+  def square_to_s
+    date_squared.to_s
+  end
+
+  def last_four_digits
+    total_string = square_to_s[-4] + square_to_s[-3] + square_to_s[-2] + square_to_s[-1]
+    total_string
+  end
+
+  def offset_a
+    last_four_digits[0].to_i
+  end
+
+  def offset_b
+    last_four_digits[1].to_i
+  end
+
+  def offset_c
+    last_four_digits[2].to_i
+  end
+
+  def offset_d
+    last_four_digits[3].to_i
   end
 
   def shift_a
