f25dd5f7d1da8c818a817d842c88d31c15007b15
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 11:08:06 -0700

    Integrate: a_key method

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -2,8 +2,17 @@ require 'securerandom'
 
 class KeyGenerator
 
+  def initialize
+    @key = nil
+  end
+
 
   def create_key
-    SecureRandom.random_number(99999).to_s.rjust(5, "0")
+    @key = SecureRandom.random_number(99999).to_s.rjust(5, "0")
   end
+
+  def a_key
+    (@key[0] + @key[1]).to_i
+  end
+
 end
