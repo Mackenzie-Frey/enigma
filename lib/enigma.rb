a5a867a8a15d5107e4e9058b485ea43002c7adbd
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 10:34:37 -0700

    Refactor @alphabet

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,7 +9,7 @@ class Enigma
   attr_reader :alphabet
 
   def initialize
-    @alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    @alphabet = ("a".."z").to_a << " "
   end
 
   def encrypt(message, key = key_generator.create_key, date = Date.today.strftime('%d%m%y'))
