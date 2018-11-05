b478c0fb3101bfc2fe49a5b8ca9d39efde4b09a0
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 14:33:27 -0700

    Test: it_knows_shifts

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -8,11 +8,25 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
-  def test_it_has_shift_array
+  def test_it_has_empty_shift_array
     shift  = Shift.new
-    actual = shift.shiftamount
+    actual = shift.shift_amounts
     assert_equal [], actual
   end
+
+  def test_it_knows_shifts
+    shift = Shift.new
+    assert_instance_of Array, shift.shift_amounts
+
+    actual   = shift.shift_amounts.length
+    expected = 4
+    assert_equal expected, actual
+
+    actual = shift.shift_amounts.first
+    assert_instance_of Integer, actual
+  end
+
+
 
 
 end
