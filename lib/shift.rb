915c7968ecbed56693afd4219b3970607782d7c7
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 14:40:45 -0700

    experiementing with shift class. About to move offset class and key generator into shift class

--- lib/shift.rb
+++ lib/shift.rb
@@ -2,4 +2,31 @@ require './lib/key_generator'
 require './lib/offset'
 
 class Shift
+
+  def initialize
+    @shift_amounts = []
+  end
+
+  def shift_a
+    binding.pry
+    offset.offset_a + key.a_key
+  end
+
+  def shift_b
+
+  end
+
+  def shift_c
+
+  end
+
+  def shift_d
+
+  end
+
+  def shift_amounts
+    shift_a
+    @shift_amounts
+  end
+
 end
