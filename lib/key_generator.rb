4edeaeea8ec4821edddbd11eb9b1f454d52215ed
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 11:34:14 -0700

    Integrate: b_key, c_key, d_key

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -12,4 +12,16 @@ class KeyGenerator
     (@key[0] + @key[1]).to_i
   end
 
+  def b_key
+    (@key[1] + @key[2]).to_i
+  end
+
+  def c_key
+    (@key[2] + @key[3]).to_i
+  end
+
+  def d_key
+    (@key[3] + @key[4]).to_i
+  end
+
 end
