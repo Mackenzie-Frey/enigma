b7398b4b2acf9ee340494b9f1774f7d4f3e92726
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 13:53:35 -0700

    Integrate: Offset_a,b,c,d

--- lib/offset.rb
+++ lib/offset.rb
@@ -12,18 +12,28 @@ class Offset
     @date.to_i ** 2
   end
 
-  def date_squared_to_s
+  def square_to_s
     date_squared.to_s
   end
 
   def last_four_digits
-    total_string = date_squared_to_s[-4] + date_squared_to_s[-3] + date_squared_to_s[-2] + date_squared_to_s[-1]
+    total_string = square_to_s[-4] + square_to_s[-3] + square_to_s[-2] + square_to_s[-1]
     total_string
   end
 
-end
+  def offset_a
+    last_four_digits[0].to_i
+  end
 
-# A offset: The first digit (1)
-# B offset: The second digit (0)
-# C offset: The third digit (2)
-# D offset: The fourth digit (5)
+  def offset_b
+    last_four_digits[1].to_i
+  end
+
+  def offset_c
+    last_four_digits[2].to_i
+  end
+
+  def offset_d
+    last_four_digits[3].to_i
+  end
+end
