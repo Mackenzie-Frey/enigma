adf584a9969be881f93ed9b271ce6395b2e9fbc5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 15:30:38 -0700

    Delete: Key_Generator.rb consolidate into shift class

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,15 +1,33 @@
-require './lib/key_generator'
-require './lib/offset'
+require 'securerandom'
 
 class Shift
 
+  attr_reader :key
+
   def initialize
+    @key = SecureRandom.random_number(99999).to_s.rjust(5, "0")
     @shift_amounts = []
+  end
+
+  def a_key
+    (@key[0] + @key[1]).to_i
+  end
+
+  def b_key
+    (@key[1] + @key[2]).to_i
+  end
+
+  def c_key
+    (@key[2] + @key[3]).to_i
+  end
+
+  def d_key
+    (@key[3] + @key[4]).to_i
   end
 
   def shift_a
     binding.pry
-    offset.offset_a + key.a_key
+    .offset_a + key.a_key
   end
 
   def shift_b
@@ -28,5 +46,4 @@ class Shift
     shift_a
     @shift_amounts
   end
-
 end
