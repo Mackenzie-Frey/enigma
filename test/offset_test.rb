d4b130ee95dc345e9222e29625ed3d50d1d7fb27
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-05 12:11:39 -0700

    Test: reformats_date_to_DDMMYY

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -8,9 +8,12 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, offset
   end
 
-  # def test_it_
-  #   enigma = Enigma.new
-  #   enigma.alphabet
-  # end
+  def test_it_reformats_date_to_DDMMYY
+    offset = Offset.new
+    date = Date.new
+    actual = date.reformat_date
+    expected =
+    assert_equal expected, actual
+  end
 
 end
