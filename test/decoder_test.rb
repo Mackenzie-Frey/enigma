2b9a55b02eb8b7db063c6209970e0a87078fe546
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-06 16:36:45 -0700

    change expected values and decoder class argument

--- test/decoder_test.rb
+++ test/decoder_test.rb
@@ -8,7 +8,7 @@ class DecoderTest < Minitest::Test
   end
 
   def test_it_has_an_alphabet
-    decoder = Decoder.new("Hello World")
+    decoder = Decoder.new("keder ohulw")
     expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
     assert_equal expected, decoder.alphabet
   end
@@ -21,31 +21,31 @@ class DecoderTest < Minitest::Test
   end
 
   def test_it_decodes_one_letter
-    decoder  = Decoder.new("a")
-    assert_equal "c", decoder.decode([2])
+    decoder  = Decoder.new("c")
+    assert_equal "a", decoder.decode([2])
   end
 
   def test_it_can_decode_word
-    decoder = Decoder.new("ab")
-    assert_equal "cd", decoder.decode([2])
+    decoder = Decoder.new("cd")
+    assert_equal "ab", decoder.decode([2])
   end
 
   def test_it_can_decode_phrase
-    decoder  = Decoder.new("ab ab")
-    assert_equal "cdbcd", decoder.decode([2])
+    decoder  = Decoder.new("cdbcd")
+    assert_equal "ab ab", decoder.decode([2])
   end
 
   def test_special_characters_persist
-    decoder  = Decoder.new("!% a")
+    decoder  = Decoder.new("!%bc")
     actual   = decoder.decode([2])
-    expected = "!%bc"
+    expected = "!% a"
     assert_equal expected, actual
   end
 
   def test_it_takes_shifts_as_an_array
-    decoder  = Decoder.new("Hello World")
+    decoder  = Decoder.new("keder ohulw")
     actual   = decoder.decode([3, 27, 73, 20])
-    expected = 'keder ohulw'
+    expected = 'hello world'
     assert_equal expected, actual
   end
 
