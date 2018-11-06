2ce7fe87e248fc5181e80c49d1f8b264659bf10f
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-11-06 07:43:35 -0700

    Test: splits_into_an_array & can_downcase

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -12,5 +12,30 @@ class EncryptTest < Minitest::Test
     expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
     assert_equal expected, encrypt.alphabet
   end
-  
+
+  def test_it_splits_into_an_array
+    encrypt = Encrypt.new
+    actual = encrypt.encode("hello world")
+    expected = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
+    assert_equal expected, actual
+  end
+
+  def test_it_can_downcase
+    encrypt = Encrypt.new
+    actual = encrypt.encode("Hello wOrld")
+    expected = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
+    assert_equal expected, actual
+  end
+
+
+  def test_characters_not_in_alphabet_persist
+skip
+    encrypt = Encrypt.new
+    actual = encrypt.encode("Hello wOrld!%()")
+    expected = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
+    assert_equal expected, actual
+  end
+
+  #Enumerator#with_index
+# sentence.split("")
 end
